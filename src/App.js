af46730954aacd710575ac7a9deca9822a5284ce
Christina Rontell <clrontell@gmail.com>
2023-01-23 12:32:10 -0600

    Header component setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <div className='container'>
+        <Header/>
+      </div>
+    </>
   );
 }
 
